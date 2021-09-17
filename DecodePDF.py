64fa12a3b3ce29d93261b0666cb8ad238c9ff4de
vgus <uvkoking@gmail.com>
2021-09-17 12:50:20 -0500

    modify to take arguments from command line

--- DecodePDF.py
+++ DecodePDF.py
@@ -1,6 +1,10 @@
 import base64
+from sys import argv
 
-with open('test.txt','rb') as codificado, open("Nuevo2.pdf", "wb") as pdf_file:
+file = argv[1]
+fileDest = argv[2]
+
+with open(file,'rb') as codificado, open(fileDest, "wb") as pdf_file:
     decoded_string = base64.b64decode(codificado.read())
     #print(encoded_string)
     pdf_file.write(decoded_string)
--- EncodePDF.py
+++ EncodePDF.py
@@ -1,28 +1,25 @@
 import base64
+from sys import argv
 import qrcode
 
-file = "test.pdf"
-#file = "PrinpipianteTypping.pdf"
+file = argv[1]
 chunk = 2200
+destination = 'mail@example.com'
 
 with open(file, "rb") as pdf_file, open('archivoab64.txt','wb') as codificado:
     encoded_string = base64.b64encode(pdf_file.read())
-    #encoded_string = base64.a85encode(pdf_file.read())
-    
     codificado.write(encoded_string)
 
 toQr = encoded_string.decode('UTF-8')
 
 chunkInit = 0
 for i,chunkNum in enumerate(range(chunk,len(toQr)+chunk-1,chunk)):
-    mail = f'mailto:uvkoking@gmail.com?subject={file}&body={i}\n'  
+    mail = f'mailto:{destination}?subject={file}&body={i}\n'  
     img = qrcode.make(mail+toQr[chunkInit:chunkNum])
     chunkInit = chunkNum
-    nameEx = f'{file}{i}.png'
+    nameEx = f'QrsIms/{file}{i}.png'
     f = open(nameEx, "wb")
     img.save(f)
     f.close()
     
-
-#print(toQr)
 
